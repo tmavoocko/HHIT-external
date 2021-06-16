05a3ea972f8d8a72a9cd90c60c70dfbeeefcceb3
tmavoocko <42184693+tmavoocko@users.noreply.github.com>
2021-06-16 16:36:04 +0200

    Add files via upload

---
+++ -New userscript-.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.optika-oko.cz/produkty/
+// @icon         https://www.google.com/s2/favicons?domain=optika-oko.cz
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    alert('hi');
+})();
